3f7537459699c85cc6e0d2d955ee1f35e4f0181a
Anant Narayan <anantnrg@gmail.com>
2024-03-16 22:28:22 +0530

    fix scrolling, add cool blur to logo and customize scrollbar

--- app.vue
+++ app.vue
@@ -23,4 +23,26 @@ html,
 body {
   overflow: hidden;
 }
+
+::-webkit-scrollbar {
+  width: 16px;
+  height: 16px;
+}
+
+::-webkit-scrollbar-track {
+  background-color: #1e1d2d;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #45475a;
+  border-radius: 40px;
+  border: 6px solid transparent;
+  background-clip: padding-box;
+  transition: border-width 0.5s ease-in-out;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  border: 5px solid transparent;
+  background-color: #f38ba8;
+}
 </style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,12 @@
 <template>
     <div
         class="w-full h-[56px] backdrop-filter backdrop-blur-sm backdrop-saturate-150 flex items-center content-between px-8 select-none sticky top-0 relative">
-        <div class="fg-text mr-auto flex h-full items-center justify-center">
-            <NuxtImg src="/strata_logo.png" width="40px"
-                class="hover:scale-[1.2] transition ease-in-out duration-384 active:scale-[0.9]" />
+        <div class="fg-text mr-auto flex h-full items-center justify-center relative items-center">
+            <div class="relative bg-red logo-cont">
+                <div class="w-[40px] h-[40px] rounded-full logo-bkg absolute top-0 left-0 -translate-y-2/4" />
+                <NuxtImg src="/strata_logo.png" width="40px"
+                    class="logo hover:scale-[1.2] transition ease-in-out duration-384 active:scale-[0.9] absolute top-0 left-0 -translate-y-2/4" />
+            </div>
         </div>
         <div class="ml-auto h-full flex items-center content-center">
             <div class="flex gap-3">
@@ -27,3 +30,16 @@
         </div>
     </div>
 </template>
+
+<style>
+.logo-bkg {
+    background-image: linear-gradient(35deg, #E384FF, 20%, #5800FF);
+    filter: blur(20px);
+    opacity: 0.5;
+    transition: opacity 386ms ease-in-out;
+}
+
+.logo-cont:hover .logo-bkg {
+    opacity: 1;
+}
+</style>
